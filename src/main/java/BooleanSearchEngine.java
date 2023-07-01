699a00d5aa82ff983c332e22574ace08f441bb64
Sergei Berketov <sergio_berketov@mail.ru>
2023-07-02 01:40:57 +0700

    questions 1

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,51 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.SimpleTextExtractionStrategy;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+
+   Map<String, PageEntry> infoAboutWord = new HashMap<>();
+
+   Object pageEntryForSearch = new Object();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+
+        try (var document = new PdfDocument(new PdfReader(pdfsDir))) {
+            var strategy = new SimpleTextExtractionStrategy();
+            for (int i = 1; i < document.getNumberOfPages(); i++) {
+                String text = PdfTextExtractor.getTextFromPage(document.getPage(i), strategy);
+                var words = text.split("\\P{IsAlphabetic}+");
+
+                Map<String, Integer> wordsCounter = new HashMap<>();
+                for (var word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    wordsCounter.put(word, wordsCounter.getOrDefault(word, 0) + 1);
+
+                    PageEntry currentPageEntry = new PageEntry(pdfsDir.getName(), document.getNumberOfPages(), wordsCounter. );
+                    infoAboutWord.put(word, currentPageEntry);
+                    pageEntryForSearch = currentPageEntry;
+                }
+            }
+        }
     }
+
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
+        if (infoAboutWord.containsKey(word)){
+
+            System.out.println(pageEntryForSearch.toString());
+
+        }
         return Collections.emptyList();
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,39 @@
+import java.util.Comparator;
+
 public class PageEntry implements Comparable<PageEntry> {
     private final String pdfName;
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(PageEntry entryCount) {
+        return (this.count - entryCount.page);
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "\n\tPDF='" + pdfName + "'," +
+                "\n\tpage='" + page + "'," +
+                "\n\tcount'" + count + "'," +
+                '}';
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
