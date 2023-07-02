f538fb9be1597ecd5af4b63f5ae2ebd8a8870bd8
Sergei Berketov <sergio_berketov@mail.ru>
2023-07-02 12:54:18 +0700

    Second question

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -10,9 +10,9 @@ import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
 
-   Map<String, PageEntry> infoAboutWord = new HashMap<>();
+    Map<String, List<PageEntry>> infoAboutWord = new HashMap<>();
 
-   Object pageEntryForSearch = new Object();
+
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
 
@@ -22,30 +22,29 @@ public class BooleanSearchEngine implements SearchEngine {
                 String text = PdfTextExtractor.getTextFromPage(document.getPage(i), strategy);
                 var words = text.split("\\P{IsAlphabetic}+");
 
-                Map<String, Integer> wordsCounter = new HashMap<>();
+                Map<String, Integer> wordAndCount = new HashMap<>();
                 for (var word : words) {
                     if (word.isEmpty()) {
                         continue;
                     }
                     word = word.toLowerCase();
-                    wordsCounter.put(word, wordsCounter.getOrDefault(word, 0) + 1);
+                    wordAndCount.put(word, wordAndCount.getOrDefault(word, 0) + 1);
 
-                    PageEntry currentPageEntry = new PageEntry(pdfsDir.getName(), document.getNumberOfPages(), wordsCounter. );
-                    infoAboutWord.put(word, currentPageEntry);
-                    pageEntryForSearch = currentPageEntry;
+                    PageEntry currentPageEntry = new PageEntry(pdfsDir.getName(), document.getNumberOfPages(),
+                            wordAndCount.getOrDefault(word, 0) + 1);
+
+                    infoAboutWord.put(word, (List<PageEntry>) currentPageEntry);
                 }
             }
         }
     }
 
-
     @Override
     public List<PageEntry> search(String word) {
-        if (infoAboutWord.containsKey(word)){
-
-            System.out.println(pageEntryForSearch.toString());
-
-        }
-        return Collections.emptyList();
+        List<PageEntry> result = infoAboutWord.get(word).stream()
+                .sorted(PageEntry::compareTo)
+                .collect(Collectors.toList());
+        result.toString();
+        return result;
     }
 }
