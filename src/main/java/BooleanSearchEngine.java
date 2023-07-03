6c3cf33810f233253cc77e3eaada7a0493d4ecce
Sergei Berketov <sergio_berketov@mail.ru>
2023-07-03 19:26:41 +0700

    4 commit

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -4,33 +4,48 @@ import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 import com.itextpdf.kernel.pdf.canvas.parser.listener.SimpleTextExtractionStrategy;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import static com.itextpdf.kernel.pdf.PdfName.Path;
 
 public class BooleanSearchEngine implements SearchEngine {
     Map<String, List<PageEntry>> infoAboutWord = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
 
-        try (var document = new PdfDocument(new PdfReader(pdfsDir))) {
-            var strategy = new SimpleTextExtractionStrategy();
-            for (int i = 1; i < document.getNumberOfPages(); i++) {
-                String text = PdfTextExtractor.getTextFromPage(document.getPage(i), strategy);
-                var words = text.split("\\P{IsAlphabetic}+");
+        File dir = new File(pdfsDir.toURI());
+        if (dir.isDirectory()) {
+            for (File item : dir.listFiles()) {
+                if (item.isFile()) {
 
-                Map<String, Integer> wordAndCount = new HashMap<>();
-                for (var word : words) {
-                    if (word.isEmpty()) {
-                        continue;
+                    try (var document = new PdfDocument(new PdfReader(item))) {
+
+                        var strategy = new SimpleTextExtractionStrategy();
+                        for (int i = 1; i < document.getNumberOfPages(); i++) {
+                            String text = PdfTextExtractor.getTextFromPage(document.getPage(i), strategy);
+                            var words = text.split("\\P{IsAlphabetic}+");
+
+                            Map<String, Integer> wordAndCount = new HashMap<>();
+                            for (var word : words) {
+                                if (word.isEmpty()) {
+                                    continue;
+                                }
+                                word = word.toLowerCase();
+                                wordAndCount.put(word, wordAndCount.getOrDefault(word, 0) + 1);
+
+                                PageEntry currentPageEntry = new PageEntry(item.getName(), document.getNumberOfPages(),
+                                        wordAndCount.getOrDefault(word, 0) + 1);
+                                List<PageEntry> allEntries = infoAboutWord.getOrDefault(word, new ArrayList<>());
+                                allEntries.add(currentPageEntry);
+                                infoAboutWord.put(word, allEntries);
+                            }
+                        }
                     }
-                    word = word.toLowerCase();
-                    wordAndCount.put(word, wordAndCount.getOrDefault(word, 0) + 1);
-
-                    PageEntry currentPageEntry = new PageEntry(pdfsDir.getName(), document.getNumberOfPages(),
-                            wordAndCount.getOrDefault(word, 0) + 1);
-
-                    infoAboutWord.put(word, (List<PageEntry>) currentPageEntry);
                 }
             }
         }
